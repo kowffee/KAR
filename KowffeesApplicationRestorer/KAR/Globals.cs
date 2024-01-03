52c0978f2a0e70e981fff22ba03d0e4676b885c5
kowffee <155503518+kowffee@users.noreply.github.com>
2024-01-02 22:17:58 -0500

    Now using Task.WhenAll

---
+++ KowffeesApplicationRestorer/KAR/Globals.cs
@@ -0,0 +1,7 @@
+﻿namespace KAR
+{
+    internal class Globals
+    {
+        internal static List<(string Url, string fileName)> AppList = new List<(string Url, string fileName)>();
+    }
+}
--- KowffeesApplicationRestorer/KAR/Program.cs
+++ KowffeesApplicationRestorer/KAR/Program.cs
@@ -1,6 +1,5 @@
 ﻿using static System.ConsoleColor;
 using static KAR.Utils.Printer;
-using static KAR.Utils.Network;
 
 namespace KAR
 {
@@ -17,13 +16,7 @@ namespace KAR
                 Environment.Exit(0);
             }
 
-            // Might improve this but not rlly needed rn
-            await DownloadFile("https://github.com/brave/brave-browser/releases/latest/download/BraveBrowserSetup.exe", "BraveInstaller64bit.exe");
-            await DownloadFile("https://laptop-updates.brave.com/latest/winx64/beta?bitness=64", "BraveBetaInstaller64bit.exe");
-            await DownloadFile("https://laptop-updates.brave.com/latest/winx64/nightly?bitness=64", "BraveNightlyInstaller64bit.exe");
-            await DownloadFile("https://openrgb.org/releases/release_0.9/OpenRGB_0.9_Windows_64_b5f46e3.zip", "OpenRGB.zip");
-            await DownloadFile("https://github.com/Vencord/Vesktop/releases/download/v0.4.4/Vesktop-Setup-0.4.4.exe", "VesktopV0.4.4Setup.exe");
-            await DownloadFile("https://central.github.com/deployments/desktop/desktop/latest/win32", "GithubInstaller.exe");
+            await Handlers.StartDownload();
 
             Print("Done!", Green);
             Console.ReadKey();
